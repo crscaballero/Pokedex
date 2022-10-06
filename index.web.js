661aa98cb368d2c249bd2cee012b528f11c2b1fd
Cristian Caballero <48134692+crscaballero@users.noreply.github.com>
2022-10-06 14:24:14 -0700

    (core) Fixed icons for web

--- index.web.js
+++ index.web.js
@@ -3,6 +3,26 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import app from './app.json';
 
+/********** Start react-native-vector-icons config **********/
+import Ionicons from 'react-native-vector-icons/Fonts/Ionicons.ttf';
+
+const IoniconsStyles = `@font-face {
+  src: url(${Ionicons});
+  font-family: Ionicons;
+}`;
+
+const style = document.createElement('style');
+style.type = 'text/css';
+
+if (style.styleSheet) {
+  style.styleSheet.cssText = IoniconsStyles;
+} else {
+  style.appendChild(document.createTextNode(IoniconsStyles));
+}
+
+document.head.appendChild(style);
+/********** End react-native-vector-icons config **********/
+
 if (module.hot) {
   module.hot.accept();
 }
